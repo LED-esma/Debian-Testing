130da9c8e6a241b9de36bf8ecba9182f9fb72cc1
ramon ledesma <98295103+LED-esma@users.noreply.github.com>
2023-07-22 01:01:53 -0700

    Update build.sh
    Prevent error of this directory not appearing from happening

--- build.sh
+++ build.sh
@@ -73,7 +73,7 @@ build () {
 "
   YYYYMMDD="$(date +%Y%m%d)"
   OUTPUT_DIR="$BASE_DIR/builds/$BUILD_ARCH"
-  mkdir -p "$OUTPUT_DIR"
+  mkdir -p $OUTPUT_DIR
   FNAME="pOs-$VERSION-$CHANNEL.$YYYYMMDD$OUTPUT_SUFFIX"
   mv $BASE_DIR/tmp/amd64/${FNAME}.iso "$OUTPUT_DIR/"
   
