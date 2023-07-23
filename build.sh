064d6f3f42744a4d7491a99bd1418f354536c234
ramon ledesma <98295103+LED-esma@users.noreply.github.com>
2023-07-22 19:39:06 -0700

    Update build.sh
    funny fix??

--- build.sh
+++ build.sh
@@ -72,7 +72,7 @@ build () {
 "
   YYYYMMDD="$(date +%Y%m%d)"
   OUTPUT_DIR="$BASE_DIR/builds/$BUILD_ARCH"
-  mkdir -p "$OUTPUT_DIR"
+  mkdir -p $OUTPUT_DIR
   FNAME="pOs-$VERSION-$CHANNEL.$YYYYMMDD$OUTPUT_SUFFIX"
   mv $BASE_DIR/tmp/amd64/live-image.hybrid.iso "$OUTPUT_DIR/${FNAME}.iso"
 
